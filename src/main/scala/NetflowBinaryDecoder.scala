ec2398c7373df8806e3f4f3039602db061bdcdb2
Jan Heyd <j.heyd@mailbox.tu-berlin.de>
2016-07-27 19:33:15 +0200

    removed unnecessary loop counters

--- src/main/scala/NetflowBinaryDecoder.scala
+++ src/main/scala/NetflowBinaryDecoder.scala
@@ -12,22 +12,17 @@ object NetflowBinaryDecoder {
 
   private def load(inputFile: File, maxChunks: Option[Int]): Seq[NetflowPacket] = {
     val in = new FileInputStream(inputFile)
-    var i = 0
     val packets = mutable.Buffer[NetflowPacket]()
     val headerBytes = new Array[Byte](headerLength)
-    while (maxChunks.forall(i < _) && in.read(headerBytes) == headerLength) {
+    while (maxChunks.forall(packets.length < _) && in.read(headerBytes) == headerLength) {
       val header = new NetflowHeader(headerBytes.clone())
 
       val records = mutable.Buffer[NetflowRecord]()
       val buf = new Array[Byte](flowSize)
-      var k = 0
-      while (k < header.packetLength && in.read(buf) == flowSize) {
+      while (records.length < header.packetLength && in.read(buf) == flowSize)
         records += new NetflowRecord(buf.clone())
-        k += 1
-      }
 
       packets += new NetflowPacket(header, records.toSeq)
-      i += 1
     }
     packets
   }
