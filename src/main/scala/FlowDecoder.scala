584f7c434cbc347c6a9d172899d365cabaecbf06
Jan Heyd <j.heyd@mailbox.tu-berlin.de>
2016-07-27 18:57:40 +0200

    renamed srcIp, dstIp to srcaddr, dstaddr to be consisten with naming in netflowV5 standard

--- src/main/scala/FlowDecoder.scala
+++ src/main/scala/FlowDecoder.scala
@@ -2,9 +2,9 @@ import java.net.InetAddress
 
 object FlowDecoder {
   def decode(record: NetflowRecord): Flow = {
-    val srcIp = InetAddress.getByAddress(record.bytes.slice(0, 4).toArray).getHostAddress
-    val dstIp = InetAddress.getByAddress(record.bytes.slice(4, 8).toArray).getHostAddress
-    new Flow(srcIp, dstIp)
+    val srcaddr = InetAddress.getByAddress(record.bytes.slice(0, 4).toArray).getHostAddress
+    val dstaddr = InetAddress.getByAddress(record.bytes.slice(4, 8).toArray).getHostAddress
+    new Flow(srcaddr, dstaddr)
   }
 
 }
