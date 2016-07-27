176680860799f53bea5fdc574cb47e8c94417626
Jan Heyd <j.heyd@mailbox.tu-berlin.de>
2016-07-27 19:13:58 +0200

    improve padding method

--- src/main/scala/FlowDecoder.scala
+++ src/main/scala/FlowDecoder.scala
@@ -7,8 +7,8 @@ object FlowDecoder {
     val dstaddr = InetAddress.getByAddress(record.bytes.slice(4, 8).toArray).getHostAddress
     val dPkts = ByteBuffer.wrap(record.bytes.slice(16, 20).toArray).getInt
     val dOctets = ByteBuffer.wrap(record.bytes.slice(20, 24).toArray).getInt
-    val srcport = ByteBuffer.wrap(Array[Byte](0, 0, record.bytes(32), record.bytes(33))).getInt()
-    val dstport = ByteBuffer.wrap(Array[Byte](0, 0, record.bytes(34), record.bytes(35))).getInt()
+    val srcport = ByteBuffer.wrap(Array[Byte](0, 0) ++ record.bytes.slice(32, 34)).getInt()
+    val dstport = ByteBuffer.wrap(Array[Byte](0, 0) ++ record.bytes.slice(32, 34)).getInt()
     new Flow(srcaddr, dstaddr, dPkts, dOctets, srcport, dstport)
   }
 
