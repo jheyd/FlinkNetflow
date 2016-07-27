b513c144264552bfe4dbc717a694cdf55337ddf5
Jan Heyd <j.heyd@mailbox.tu-berlin.de>
2016-07-27 19:19:25 +0200

    extracted val recordBytes

--- src/main/scala/FlowDecoder.scala
+++ src/main/scala/FlowDecoder.scala
@@ -3,12 +3,17 @@ import java.nio.ByteBuffer
 
 object FlowDecoder {
   def decode(record: NetflowRecord): Flow = {
-    val srcaddr = InetAddress.getByAddress(record.bytes.slice(0, 4).toArray).getHostAddress
-    val dstaddr = InetAddress.getByAddress(record.bytes.slice(4, 8).toArray).getHostAddress
-    val dPkts = ByteBuffer.wrap(Array[Byte](0, 0, 0, 0) ++ record.bytes.slice(16, 20)).getLong
-    val dOctets = ByteBuffer.wrap(Array[Byte](0, 0, 0, 0) ++ record.bytes.slice(20, 24)).getLong
-    val srcport = ByteBuffer.wrap(Array[Byte](0, 0) ++ record.bytes.slice(32, 34)).getInt()
-    val dstport = ByteBuffer.wrap(Array[Byte](0, 0) ++ record.bytes.slice(32, 34)).getInt()
+    val recordBytes = record.bytes.toArray
+
+    val srcaddr = InetAddress.getByAddress(recordBytes.slice(0, 4)).getHostAddress
+    val dstaddr = InetAddress.getByAddress(recordBytes.slice(4, 8)).getHostAddress
+
+    val dPkts = ByteBuffer.wrap(Array[Byte](0, 0, 0, 0) ++ recordBytes.slice(16, 20)).getLong
+    val dOctets = ByteBuffer.wrap(Array[Byte](0, 0, 0, 0) ++ recordBytes.slice(20, 24)).getLong
+
+    val srcport = ByteBuffer.wrap(Array[Byte](0, 0) ++ recordBytes.slice(32, 34)).getInt()
+    val dstport = ByteBuffer.wrap(Array[Byte](0, 0) ++ recordBytes.slice(32, 34)).getInt()
+
     new Flow(srcaddr, dstaddr, dPkts, dOctets, srcport, dstport)
   }
 
