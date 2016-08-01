1d64266856f5f0c8e5153c41fc42b338243e6c4f
Jan Heyd <j.heyd@mailbox.tu-berlin.de>
2016-08-01 13:00:26 +0200

    replaced companion object with case class modifier

--- src/main/scala/de/tuberlin/inet/flink/netflow/binary/util/BinaryData.scala
+++ src/main/scala/de/tuberlin/inet/flink/netflow/binary/util/BinaryData.scala
@@ -2,15 +2,11 @@ package de.tuberlin.inet.flink.netflow.binary.util
 
 import java.nio.ByteBuffer
 
-class BinaryData(bytes: Seq[Byte]) {
+case class BinaryData(bytes: Seq[Byte]) {
 
   def extractUnsignedShort(offset: Int): Int = ByteBuffer.wrap(Array[Byte](0, 0) ++ bytes.slice(offset, offset + 2)).getInt()
 
   def extractUnsignedInt(offset: Int): Long = ByteBuffer.wrap(Array[Byte](0, 0, 0, 0) ++ bytes.slice(offset, offset + 4)).getLong()
 
   def extractBytes(offset: Int, count: Int): Array[Byte] = bytes.slice(offset, offset + count).toArray
-}
-
-object BinaryData {
-  def apply(bytes: Seq[Byte]): BinaryData = new BinaryData(bytes)
 }
